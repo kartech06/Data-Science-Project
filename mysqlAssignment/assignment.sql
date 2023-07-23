8ccab5f06ecdaded7ee4b0e57fb26df1447776c3
Kartik Mehta <59331734+kartech06@users.noreply.github.com>
2023-07-23 21:30:05 +0530

    Update assignment.sql

--- mysqlAssignment/assignment.sql
+++ mysqlAssignment/assignment.sql
@@ -1,13 +1,16 @@
-
+## Creating a Database
 Create database assignment_db;
 
+## Using DB
 use assignment_db;
 
+## Creating DB
 CREATE TABLE store_sales(
 month varchar(20),
 prod_name varchar(25),
 sales int)
 
+## Inserting values
 INSERT into store_sales(month,prod_name,sales) VALUES ('Jan','Fruits',45000),
 ('Jan','Vegetables',67000),
 ('Jan','Dairy',55000),
@@ -18,6 +21,7 @@ INSERT into store_sales(month,prod_name,sales) VALUES ('Jan','Fruits',45000),
 ('Jan','Vegetables',43000),
 ('Jan','Dairy',92000);
 
+## Printing Values
 select * from store_sales;
 
 
