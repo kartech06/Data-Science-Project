e33c5ff21ef5570866477267b294bcb45573807e
Kartik Mehta <59331734+kartech06@users.noreply.github.com>
2023-07-23 21:28:25 +0530

    Add files via upload

---
+++ mysqlAssignment/assignment.sql
@@ -0,0 +1,23 @@
+
+Create database assignment_db;
+
+use assignment_db;
+
+CREATE TABLE store_sales(
+month varchar(20),
+prod_name varchar(25),
+sales int)
+
+INSERT into store_sales(month,prod_name,sales) VALUES ('Jan','Fruits',45000),
+('Jan','Vegetables',67000),
+('Jan','Dairy',55000),
+('Feb','Fruits',42000),
+('Feb','Vegetables',32000),
+('Feb','Dairy',52000),
+('Jan','Fruits',61000),
+('Jan','Vegetables',43000),
+('Jan','Dairy',92000);
+
+select * from store_sales;
+
+
