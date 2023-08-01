bba1166dbbed94f75aebfa1d41ed93dda69383e6
Kartik Mehta <59331734+kartech06@users.noreply.github.com>
2023-08-01 08:10:52 +0530

    Update assignment2.sql

--- mysqlAssignment/assignment2.sql
+++ mysqlAssignment/assignment2.sql
@@ -5,6 +5,7 @@ use assignment_db;
 Create table A(
 stud_id varchar(50),
 user_name varchar(20),
+Occupation varchar(20),
 Address varchar(20),
 State varchar(20),
 Country varchar(20)
@@ -13,9 +14,12 @@ Country varchar(20)
 ##Insert values in A
 
 insert into A values 
-(UUID(),'Chandler','15 Yemen Road', ,'Yemen','Yemen'),
-(UUID(),'Monica','Apartment 52','New Jersey','USA'),
-(UUID(),'Joey','Apartment 53','New Jersey','USA');
+(UUID(),'Chandler','Transponster','15 Yemen Road','Yemen','Yemen'),
+(UUID(),'Monica','Chef','Apartment 52','New Jersey','USA'),
+(UUID(),'Joey','Actor','Apartment 53','New Jersey','USA'),
+(UUID(),'Ross','Dinosaurs','15 Yemen Road','Yemen','Yemen'),
+(UUID(),'Rachel','Faishon','Apartment 52','New Jersey','USA'),
+(UUID(),'Phoebe','Masseuse','Apartment 53','New Jersey','USA');
 
 Select * from A;
 
@@ -24,6 +28,8 @@ Select * from A;
 Create table B(
 B_id varchar(50),
 A_id varchar(50),
+name varchar(20),
+Country varchar(20),
 hobby varchar(20),
 occupation varchar(20),
 superpower varchar(30)
@@ -31,12 +37,12 @@ superpower varchar(30)
 
 ##Insert values in B
 insert into B values 
-(UUID(),'f9556ded-2d33-11ee-aef0-6c02e0974069','Dinosaurs','Professor','Getting married'),
-(UUID(),'f95639f8-2d33-11ee-aef0-6c02e0974069','Shopping','Fashion', 'Crying'),
-(UUID(),'f9563d44-2d33-11ee-aef0-6c02e0974069','Singing','masseuse', 'psychic');
+(UUID(),'6358b7da-3013-11ee-9c1a-6c02e0974165','Ross','USA','Dinosaurs','Professor','Getting married'),
+(UUID(),'6358de92-3013-11ee-9c1a-6c02e0974165','Rachel','USA','Shopping','Fashion', 'Crying'),
+(UUID(),'6358e08a-3013-11ee-9c1a-6c02e0974165','Phoebe','USA','Singing','masseuse', 'psychic');
 
 ## a) Right join
-Select * from A Right join B on A.stud_id=B.A_id;
+Select * from B Right join A on A.stud_id=B.A_id;
 
 ## b) Left join
 Select * from A Left join B on A.stud_id=B.A_id;
@@ -48,13 +54,13 @@ Select * from A Inner join B on A.stud_id=B.A_id;
 Select * from A where A.stud_id in (select A_id from B);
 
 ## e) Union
-Select * from A 
+Select user_name,Occupation,Country from A 
 Union
-Select * from B;
+Select name,occupation,country from B;
 
 ## f) Union All
-Select * from A 
+Select user_name,Occupation,Country from A 
 Union all
-Select * from B;
+Select name,occupation,country from B;
 
 
